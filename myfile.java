6d7961a89c21bce8fee538a37096affe123d9bbc
sakshi <sakshichavhan4040@gmail.com>
2025-07-31 03:54:11 +0000

    mycommit2

--- myfile.java
+++ myfile.java
@@ -1,7 +1,13 @@
 public class myfile {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello, good morning!");
     }
 }
 
 
+=======
+        System.out.println("Hello, World!");
+    }
+}
+>>>>>>> 7f2a385 (mycommit1)
